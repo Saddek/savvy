48a4986f78800f5f384e80ed26bd798fe8eabc5a
Oliver Moran <oliver.moran@gmail.com>
2013-11-23 02:02:17 +0000

    add LOAD event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 /* This script will be available on every screen as a child of the window object (see data/app.xml). */
 
+Savvy.subscribe(Savvy.LOAD, function(){
+	// fired once when Savvy loads first
+});
+
 Savvy.subscribe(Savvy.READY, function(){
 	// fired every time a screen "ready" event is published
 });
