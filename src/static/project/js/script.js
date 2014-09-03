260410c351fe1b9d6e2d62b9339d3fa4e9e987e0
Oliver Moran <oliver.moran@gmail.com>
2014-09-04 00:02:16 +0100

    re-enable swipe

--- src/static/project/js/script.js
+++ src/static/project/js/script.js
@@ -1,6 +1,6 @@
 // when the application is ready, set up touch events
 application.addEventListener(Application.READY, function(e) {
-    /*
+
     var hammertime = new Hammer(application.main, {});
     hammertime.on("swipeleft", function(ev) {
         application.offCanvas("0px");
@@ -8,7 +8,6 @@ application.addEventListener(Application.READY, function(e) {
     hammertime.on("swiperight", function(ev) {
         application.offCanvas(Transition.OFF_CANVASS_LEFT);
     });
-    */
     
     var links = application.querySelectorAll("nav a");
     for (var i = 0; i < links.length; i++) {
