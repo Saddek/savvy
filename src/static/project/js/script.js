90424dcdde28121e75f85ca064e10ff3ef8b29b0
Oliver Moran <oliver.moran@gmail.com>
2014-09-07 01:35:28 +0100

    add comments in script

--- src/static/project/js/script.js
+++ src/static/project/js/script.js
@@ -1,6 +1,9 @@
+// When the application starts (Application.READY) and everytime we exit 
+// a card, set the correctly highlighted footer button
 application.addEventListener(Application.READY, highlightFooterButton);
 application.addEventListener(Card.EXIT, highlightFooterButton);
 
+// sets the correctly highlighted footer button
 function highlightFooterButton(event) {
     var buttons = application.footer.querySelectorAll("button");
     
@@ -11,7 +14,8 @@ function highlightFooterButton(event) {
     var button = [Home, Code].indexOf(event.detail.to);
     buttons[button].setAttribute("data-selected", "true");
 }
-                             
+                
+// Every time we ender a card, set the header title to the title of the card
 application.addEventListener(Card.ENTER, function (event) {
     var title = application.header.querySelector("h1");
     title.innerHTML = application.currentCard.title;
