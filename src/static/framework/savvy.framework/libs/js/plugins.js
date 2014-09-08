0a7882eb98c747875b4c7db05ddd68d10f9d517f
Oliver Moran <oliver.moran@gmail.com>
2014-09-08 23:55:11 +0100

    add Thumbs.js for non-touch devices

--- src/static/framework/savvy.framework/libs/js/plugins.js
+++ src/static/framework/savvy.framework/libs/js/plugins.js
@@ -64,3 +64,8 @@ if(0<=navigator.userAgent.indexOf("Android")){var from=navigator.userAgent.index
 
 // Add a "svg" class to the HTML tag if device supports SVG
 "undefined"!=typeof SVGSVGElement&&(document.documentElement.className+=" svg");
+
+// Thumbs.js - 0.6.0 - Polyfills touch events on desktops
+// Copyright (c) 2010 Michael Brooks - MIT License
+// http://mwbrooks.github.io/thumbs.js/download/thumbs.0.6.0.min.js
+(function(c){try{document.createEvent("TouchEvent");return}catch(f){}var d={mousedown:"touchstart",mouseup:"touchend",mousemove:"touchmove"};var a=function(){for(var e in d){document.body.addEventListener(e,function(i){var h=b(d[i.type],i);i.target.dispatchEvent(h);var g=i.target["on"+d[i.type]];if(typeof g==="function"){g(i)}},false)}};var b=function(g,i){var h=document.createEvent("MouseEvents");h.initMouseEvent(g,i.bubbles,i.cancelable,i.view,i.detail,i.screenX,i.screenY,i.clientX,i.clientY,i.ctrlKey,i.altKey,i.shiftKey,i.metaKey,i.button,i.relatedTarget);return h};if(document.readyState==="complete"||document.readyState==="loaded"){a()}else{c.addEventListener("load",a,false)}})(window);
