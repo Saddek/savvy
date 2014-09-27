1cf2212fe5e2c4c8577fedee58a675ef06e05647
Oliver Moran <oliver.moran@gmail.com>
2014-09-27 21:49:18 +0100

    finalise basic project

--- src/static/project/js/script.js
+++ src/static/project/js/script.js
@@ -1,3 +1,9 @@
+// Every time we ender a card, set the header title to the title of the card
+application.addEventListener(Card.ENTER, function (event) {
+    var title = application.header.querySelector("h1");
+    title.innerHTML = application.currentCard.title;
+});
+
 // When the application starts (Application.READY) and everytime we exit 
 // a card, set the correctly highlighted footer button
 application.addEventListener(Application.READY, highlightFooterButton);
@@ -11,12 +17,26 @@ function highlightFooterButton(event) {
         buttons[i].removeAttribute("data-selected");
     }
     
-    var button = [Home, Code].indexOf(event.detail.to);
+    var button = [Home, Cordova].indexOf(event.detail.to);
     buttons[button].setAttribute("data-selected", "true");
 }
-                
-// Every time we ender a card, set the header title to the title of the card
-application.addEventListener(Card.ENTER, function (event) {
-    var title = application.header.querySelector("h1");
-    title.innerHTML = application.currentCard.title;
+
+// When the application starts, apply a basic swipe listener to
+// open of close the off-canvas element
+application.addEventListener(Application.READY, function (event) {
+    var x, t;
+    application.main.addEventListener("touchstart", function(e){
+        x = e.changedTouches[0].screenX;
+        t = new Date().getTime();
+    });
+
+    application.main.addEventListener("touchend", function(e){
+        var d = e.changedTouches[0].screenX - x;
+        var e = (new Date().getTime()) - t;
+        
+        if (Math.abs(d) < 50) return;
+        if (e > 300) return;
+        if (d < 0) application.offCanvas(Transition.OFF_CANVASS_NONE);
+        else application.offCanvas(Transition.OFF_CANVASS_LEFT);
+    });
 });
